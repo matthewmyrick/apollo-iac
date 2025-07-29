008587a5638569c369b0ea0657dfac467e61c24a
matthewmyrick <matthewmyrick2@gmail.com>
2025-07-28 23:09:42 -0400

    Update

--- setup/01-system-packages.sh
+++ setup/01-system-packages.sh
@@ -13,5 +13,7 @@ sudo apt update && sudo apt upgrade -y
 # Install essential packages
 echo "Installing essential packages..."
 sudo apt install -y curl wget openssh-server ufw fail2ban git zsh eza fd-find fzf yazi zsh-autosuggestions zsh-syntax-highlighting bat
-
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
 echo "System packages installation complete!"
+
