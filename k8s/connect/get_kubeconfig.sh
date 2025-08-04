6fe79f78525609f0aa2ba1c5fdeb90875825ac57
Matthew Myrick <matthewmyrick2@gmail.com>
2025-08-04 17:11:29 -0400

    Update to get config form server

---
+++ k8s/connect/get_kubeconfig.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e # Exit on error
+# this script needs to be run on the k3s server node
+
+# Get the Tailscale IP address
+TAILSCALE_IP=$(tailscale ip -4)
+
+# Check if we got an IP
+if [ -z "$TAILSCALE_IP" ]; then
+  echo "Error: Could not get Tailscale IP address." >&2
+  exit 1
+fi
+
+# Get the k3s config and replace the server IP
+sudo cat /etc/rancher/k3s/k3s.yaml | sed "s/127.0.0.1/$TAILSCALE_IP/"
